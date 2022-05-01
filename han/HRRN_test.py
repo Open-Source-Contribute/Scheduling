3819c2798c7d0f4cbe97f771483c815fda624e4c
Han <yulmam1@naver.com>
2022-05-01 14:46:45 +0900

    주석 제거

--- han/HRRN_test.py
+++ han/HRRN_test.py
@@ -6,42 +6,35 @@ class Process:
         self.burst_time = burst_time         
         self.waiting_time = 0 
         self.turn_around_time = 0 
-    """
-    프로세스 클래스
-    """
+
 
 
           
 def HRRN(process_list: list):
-    act=process_list.copy()#현재 완료되지 않은 프로세스들의 리스트
-    act.sort(key=lambda x: x.arrival_time)#도착 시간을 기반으로 정렬한다.
+    act=process_list.copy()
+    act.sort(key=lambda x: x.arrival_time)
     current_time=0
     merge_turn_around_time=0
     merge_waiting_time=0
-    """
-    평균을 구하기 위한 변수들
-    """
+
     while len(act)>0:
         if act[0].arrival_time>current_time:
             current_time=act[0].arrival_time
-            """
-            만약 전체 프로세스는 종료가 되지 않았는데 현재시간에 도착한 프로세스가 아무것도 없을 경우
-            현재 완료되지 않은 프로세스 리스트에서 가장 도착시간이 빠른 함수로 현재시간 설정
-            """
+
         response_ratio=-1
         loc=0
         for i in range(0, len(act)):
-            if  act[i].arrival_time<current_time:#만약 현재 도착한 프로세스가 있다면
-            temp=(act[i].burst_time+(current_time-act[i].arrival_time))/act[i].burst_time#response ratio 계산
+            if  act[i].arrival_time<current_time:
+            temp=(act[i].burst_time+(current_time-act[i].arrival_time))/act[i].burst_timeSs
             if response_ratio<temp:
                 response_ratio=temp
                 loc=i
-        act[loc].waiting_time=current_time-act[loc].arrival_time#waiting time 계산
-        current_time+=act[loc].burst_time#현재 시간에서 프로세스가 실행된 시간 만큼 더해준다.
-        act[loc].turn_around_time=current_time-act[loc].arrival_time#turn around time 계산
+        act[loc].waiting_time=current_time-act[loc].arrival_time
+        current_time+=act[loc].burst_time
+        act[loc].turn_around_time=current_time-act[loc].arrival_time#
         merge_turn_around_time+=act[loc].turn_around_time
-        merge_waiting_time+=act[loc].waiting_time#평균을 위해 더해준다.
-        act.pop(loc)#실행이 된다면 리스트에서 제외한다.
+        merge_waiting_time+=act[loc].waiting_time
+        act.pop(loc)
     for i in range(0, len(process_list)):
         print(process_list[i].process_name, " arrival time :", process_list[i].arrival_time," burst time :", process_list[i].burst_time," waiting time :", process_list[i].waiting_time," turn around time :", process_list[i].turn_around_time)
 
