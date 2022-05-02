fdbaec3fafb5a09c7a9d845e1546d28bca21284c
dongjji <y2kdj9723@naver.com>
2022-05-02 18:34:10 +0900

    mlfq code style change

--- multi_level_feedback_queue/mlfq_class.py
+++ multi_level_feedback_queue/mlfq_class.py
@@ -1,57 +1,81 @@
 from collections import deque
 
 class Process:
-    def __init__(self, process_name: str, arrival_time: int, burst_time: int):
-        self.process_name = process_name 
-        self.arrival_time = arrival_time 
+    def __init__(self, process_name: str, arrival_time: int, burst_time: int) -> None:
+        self.process_name = process_name
+        self.arrival_time = arrival_time
         self.stop_time = arrival_time # end time point or finish time point
-        self.burst_time = burst_time 
-        
-        self.waiting_time = 0 
-        self.turn_around_time = 0 
-        
+        self.burst_time = burst_time
+
+        self.waiting_time = 0
+        self.turn_around_time = 0
+
         self.complete = False
-        
+
 class MLFQ:
   """
   MLFQ(Multil Level Feedback Queue)
-  Queue1 >>> RR(Round Robin, Time-Slice = 17)
-  Queue2 >>> RR(Round Robin, Time-Slice = 25)
-  Queue3 >>> FCFS(First Come, First Served)
+  Queue1: RR(Round Robin, Time-Slice = 17)
+  Queue2: RR(Round Robin, Time-Slice = 25)
+  Queue3: FCFS(First Come, First Served)
   """
-  def __init__(self, queue, current_time):
-        self.queue = queue # unfinished process is in this queue
+  def __init__(self, queue: list[Process], current_time: int) -> None:
+        self.ready_queue = queue # unfinished process is in this ready_queue
         self.current_time = current_time # current time
-        self.sequence = deque(); # finished process is in this sequence queue
-        
-  def print_process_info_of_queue(self):
+        self.finish_queue = deque(); # finished process is in this sequence queue
+
+  def calculate_waiting_time(self) -> list[int]:
+      waiting_times = []
+      for i in range(len(self.finish_queue)):
+          waiting_times.append(self.finish_queue[i].waiting_time)
+      return waiting_times
+
+  def calculate_turnaround_time(self) -> list[int]:
+      turn_around_times = []
+      for i in range(len(self.finish_queue)):
+          turn_around_times.append(self.finish_queue[i].turn_around_time)
+      return turn_around_times
+
+  def calculate_completion_time(self) -> list[int]:
+      completion_times = []
+      for i in range(len(self.finish_queue)):
+          completion_times.append(self.finish_queue[i].stop_time)
+      return completion_times
+
+  def calculate_remaining_burst_time_of_ready_queue(self) -> list[int]:
+      remaining_burst_times = []
+      for i in range(len(self.ready_queue)):
+          remaining_burst_times.append(self.ready_queue[i].burst_time)
+      return remaining_burst_times
+
+  def print_process_info_of_queue(self) -> None:
       """
-      This method prints out information of terminated processes in sequence
+      This method prints out information of terminated processes in finish_queue
       """
       print("###########sequence of processes finished###########")
-      for i in range(len(self.sequence)):
-        print(self.sequence[i].process_name, "(",
-              "arrival time: ", self.sequence[i].arrival_time,
-              ", waiting time: ", self.sequence[i].waiting_time,
-              ", completion time: ", self.sequence[i].stop_time,
-              ", turnaround time: ", self.sequence[i].turn_around_time,
-              ", left burst time: ", self.sequence[i].burst_time,
-              ")")    
-        
-  def print_current_time(self):
+      for i in range(len(self.finish_queue)):
+        print(self.finish_queue[i].process_name, "(",
+              "arrival time: ", self.finish_queue[i].arrival_time,
+              ", waiting time: ", self.finish_queue[i].waiting_time,
+              ", completion time: ", self.finish_queue[i].stop_time,
+              ", turnaround time: ", self.finish_queue[i].turn_around_time,
+              ", remaining burst time: ", self.finish_queue[i].burst_time,
+              ")")
+
+  def print_current_time(self) -> None:
       """
       This method prints out the current time
       """
       print("###########current time: ", self.current_time, "##########")
-    
-  def update_waiting_time(self, process):
+
+  def update_waiting_time(self, process: Process) -> None:
       """
       This method updates waiting times of unfinished processes
       """
       process.waiting_time += self.current_time - process.stop_time
-          
-          
-  def FCFS(self, queue):
+
+
+  def FCFS(self, queue: list[Process]) -> list[Process]:
       """
       FCFS(First Come, First Served)
       FCFS will be applied to MLFQ's Queue3
@@ -60,17 +84,17 @@ class MLFQ:
       finished = deque(); # sequence deque of terminated process
       while (len(queue) != 0):
         cp = queue.popleft() # current process
-        
+
         # if the process' arrival time is later than current time, update current time
         if (self.current_time < cp.arrival_time):
             self.current_time += cp.arrival_time
-        
+
         # update waiting time of unfinished processes
         self.update_waiting_time(cp)
         # update current time
-        self.current_time += cp.burst_time 
+        self.current_time += cp.burst_time
         # finish the process and set the process' burst-time zero
-        cp.burst_time = 0 
+        cp.burst_time = 0
         # update the process' turnaround time because it is finished
         cp.turn_around_time = self.current_time - cp.arrival_time
         # set the finish time
@@ -81,7 +105,7 @@ class MLFQ:
       # FCFS will finish all remaining processes
       return finished
 
-  def RR(self, queue, time_slice):
+  def RR(self, ready_queue: list[Process], time_slice: int) -> list[Process]:
       """
       RR(Round Robin)
       RR will be applied to MLFQ's Queue1 and Queue2
@@ -90,13 +114,13 @@ class MLFQ:
       """
       finished = deque() # sequence deque of terminated process
       # just for 1 cycle and unfinished processes will go back to queue
-      for i in range(len(queue)): 
-          cp = queue.popleft() # current process
-          
+      for i in range(len(ready_queue)):
+          cp = ready_queue.popleft() # current process
+
           # if the process' arrival time is later than current time, update current time
           if (self.current_time < cp.arrival_time):
               self.current_time += cp.arrival_time
-          
+
           # update waiting time of unfinished processes
           self.update_waiting_time(cp)
           # if the burst time of process is bigger than time-slice
@@ -108,7 +132,7 @@ class MLFQ:
               # update end point time
               cp.stop_time = self.current_time
               # locate the process behind the queue because it is not finished
-              queue.append(cp)
+              ready_queue.append(cp)
           else:
               # use CPU for remaining burst time
               self.current_time += cp.burst_time
@@ -121,43 +145,41 @@ class MLFQ:
               cp.complete = True
               # add the process to queue that has finished queue
               finished.append(cp)
-      
-      # return finished processes queue and remaining processes queue
-      return finished, queue 
 
-  def MLFQ(self):
+      # return finished processes queue and remaining processes queue
+      return finished, ready_queue
+
+  def MLFQ(self) -> list[Process]:
       """
       MLFQ
-      Queue1, Queue2(Round Robin)
-      >>> This queue will send process that is not finished during 1 cycle with time-slice
-      Queue3(FCFS)
-      >>> This queue will finish all remaining processes
+      Queue1, Queue2(Round Robin): This queue will send process that is not finished during 1 cycle with time-slice
+      Queue3(FCFS): This queue will finish all remaining processes
       """
       # Queue1(RR)
-      finished, ready_queue = self.RR(self.queue, 17)
+      finished, self.ready_queue = self.RR(self.ready_queue, 17)
       # add finished process to sequence queue
-      self.sequence.extend(finished);
-      
+      self.finish_queue.extend(finished);
+
       # Queue2(RR)
-      finished, ready_queue = self.RR(ready_queue, 25)
+      finished, self.ready_queue = self.RR(self.ready_queue, 25)
       # add finished process to sequence queue
-      self.sequence.extend(finished);
-      
+      self.finish_queue.extend(finished);
+
       # Queue3(FCFS)
-      finished = self.FCFS(ready_queue)
+      finished = self.FCFS(self.ready_queue)
       # add finished process to sequence queue
-      self.sequence.extend(finished);   
-      
-      return self.sequence
-  
+      self.finish_queue.extend(finished);
+
+      return self.finish_queue
+
 if __name__ == "__main__":
     P1 = Process("P1", 0, 53)
     P2 = Process("P2", 0, 17)
     P3 = Process("P3", 0, 68)
     P4 = Process("P4", 0, 24)
-    
+
     queue = deque([P1, P2, P3, P4])
-    
+
     mlfq = MLFQ(queue, 0)
     sequence = mlfq.MLFQ()
     mlfq.print_process_info_of_queue()
--- multi_level_feedback_queue/mlfq_function.py
+++ multi_level_feedback_queue/mlfq_function.py
@@ -2,42 +2,42 @@ from collections import deque
 
 class Process:
     def __init__(self, process_name: str, arrival_time: int, burst_time: int):
-        self.process_name = process_name 
-        self.arrival_time = arrival_time 
+        self.process_name = process_name
+        self.arrival_time = arrival_time
         self.stop_time = arrival_time # end time point or finish time point
-        self.burst_time = burst_time 
-        
-        self.waiting_time = 0 
-        self.turn_around_time = 0 
-        
+        self.burst_time = burst_time
+
+        self.waiting_time = 0
+        self.turn_around_time = 0
+
         self.complete = False
-        
+
 def print_process_info(p):
-    print("process name: ", p.process_name, 
+    print("process name: ", p.process_name,
         "\narrival time: ", p.arrival_time,
         "\nburst time: ", p.burst_time,
-        "\nwaiting time: ", p.waiting_time, 
+        "\nwaiting time: ", p.waiting_time,
         "\nturnaround time: ", p.turn_around_time)
-    
+
 def update_waiting_time(process, current_time):
     process.waiting_time += current_time - process.stop_time
-        
-        
+
+
 def FCFS(queue, current_time):
     finished = deque(); # sequence of terminated process
     while (len(queue) != 0):
         cp = queue.popleft() # current process
-        
+
         if (current_time < cp.arrival_time):
             current_time += cp.arrival_time
-            
+
         update_waiting_time(cp, current_time)
         current_time += cp.burst_time
         cp.burst_time = 0
         cp.turn_around_time = current_time - cp.arrival_time
         cp.stop_time = current_time
         cp.complete = True
-        
+
         finished.append(cp)
     return finished, current_time
 
@@ -45,10 +45,10 @@ def RR(queue, current_time, time_slice):
     finished = deque()
     for i in range(len(queue)):
         cp = queue.popleft() # current process
-        
+
         if (current_time < cp.arrival_time):
             current_time += cp.arrival_time
-        
+
         update_waiting_time(cp, current_time)
         if (cp.burst_time > time_slice):
             current_time += time_slice
@@ -62,21 +62,21 @@ def RR(queue, current_time, time_slice):
             cp.turn_around_time = current_time - cp.arrival_time
             cp.complete = True
             finished.append(cp)
-            
+
     return finished, queue, current_time
 
 def MLFQ(queue, current_time):
     sequence = deque();
-    
+
     finished, ready_queue, current_time = RR(queue, 0, 17)
     sequence.extend(finished);
-    
+
     finished, ready_queue, current_time = RR(ready_queue, current_time, 25)
     sequence.extend(finished);
-    
+
     finished, current_time = FCFS(ready_queue, current_time)
-    sequence.extend(finished);   
-    
+    sequence.extend(finished);
+
     return sequence, current_time
 
 def print_process_info_of_queue(queue):
@@ -94,11 +94,11 @@ def test():
     P2 = Process("P2", 0, 17)
     P3 = Process("P3", 0, 68)
     P4 = Process("P4", 0, 24)
-    
+
     queue = deque([P1, P2, P3, P4])
-    
+
     sequence, current_time = MLFQ(queue, 0)
-    
+
     print_process_info_of_queue(sequence);
     print("current time: ", current_time)
 
