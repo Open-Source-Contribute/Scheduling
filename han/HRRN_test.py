e0987c9110eefcbf39b43e014088fedfa61ddcf3
yulmam <70622601+yulmam@users.noreply.github.com>
2022-05-03 22:23:22 +0900

    코드 스타일 수정 완료

--- han/HRRN_test.py
+++ han/HRRN_test.py
@@ -1,55 +1,53 @@
 
 
-class Process:
-    def __init__(self, process_name: str, arrival_time: int, burst_time: int):
-        self.process_name = process_name
-        self.arrival_time = arrival_time
-        self.burst_time = burst_time
-        self.waiting_time = 0
-        self.turn_around_time = 0
-
-
-def HRRN(process_list: list):
-    act = process_list.copy()
-    act.sort(key=lambda x: x.arrival_time)
+def calcultate_turn_around_time(
+    arrival_time: list, burst_time: list, no_of_process: int
+) -> list:
     current_time = 0
-    merge_turn_around_time = 0
-    merge_waiting_time = 0
-    while len(act) > 0:
-        if act[0].arrival_time > current_time:
-            current_time = act[0].arrival_time
-
+    finished_process_count = 0
+    finished_process = [0] * no_of_process
+    turn_around_time = [0] * no_of_process
+    sorted_arrival_time = arrival_time.copy()
+    sorted_arrival_time.sort()
+    while no_of_process > finished_process_count:
+        if sorted_arrival_time[0] > current_time:
+            current_time = sorted_arrival_time[0]
         response_ratio = -1
         loc = 0
-        for i in range(0, len(act)):
-            if act[i].arrival_time < current_time:
-                temp = (act[i].burst_time + (current_time - act[i].arrival_time)) /\
-                    act[i].burst_time
+        temp = 0
+        for i in range(0, no_of_process):
+            if finished_process[i] == 0 and arrival_time[i] <= current_time:
+                temp = (burst_time[i] + (current_time - arrival_time[i])) /\
+                    burst_time[i]
             if response_ratio < temp:
                 response_ratio = temp
                 loc = i
-        act[loc].waiting_time = current_time - act[loc].arrival_time
-        current_time += act[loc].burst_time
-        act[loc].turn_around_time = current_time - act[loc].arrival_time
-        merge_turn_around_time += act[loc].turn_around_time
-        merge_waiting_time += act[loc].waiting_time
-        act.pop(loc)
-    for i in range(0, len(process_list)):
-        print(process_list[i].process_name, " arrival time :",
-              process_list[i].arrival_time, " burst time :",
-              process_list[i].burst_time, " waiting time :",
-              process_list[i].waiting_time, " turn around time :",
-              process_list[i].turn_around_time)
-    print("average waiting time :", merge_turn_around_time / len(process_list))
-    print("average turn around time :", merge_waiting_time / len(process_list))
+        turn_around_time[loc] = current_time + burst_time[loc] - arrival_time[loc]
+        current_time += burst_time[loc]
+        finished_process[loc] = 1
+        finished_process_count += 1
+        sorted_arrival_time.remove(arrival_time[loc])
+
+    return turn_around_time
 
 
-if __name__ == "__main__":
+def calculate_waiting_time(
+    turn_around_time: list, burst_time: list, no_of_process: int
+) -> list:
+    waiting_time = [0] * no_of_process
+    for i in range(0, no_of_process):
+        waiting_time[i] = turn_around_time[i] - burst_time[i]
+    return waiting_time
 
-    p1 = Process("p1", 0, 53)
-    p2 = Process("p2", 3, 10)
-    p3 = Process("p3", 5, 13)
-    p4 = Process("p4", 7, 150)
 
-    process_list = [p1, p2, p3, p4]
-    HRRN(process_list)
+def calcuate_average_time(
+    waiting_time: list, turn_around_time: list, no_of_process: int
+) -> None:
+    merge_waiting_time = 0
+    merge_turn_around_time = 0
+    for i in range(0, no_of_process):
+        merge_waiting_time += waiting_time[i]
+        merge_turn_around_time += turn_around_time[i]
+
+    print("average waiting time :", merge_waiting_time / no_of_process)
+    print("average turn around time :", merge_turn_around_time / no_of_process)
